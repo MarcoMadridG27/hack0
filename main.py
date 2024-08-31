c90e3123276e3afffffdf7c1b64eed263e7169a3
Ivan Rodriguez <ivan.rodriguez@utec.edu.pe>
2024-08-31 17:19:07 -0500

    Multi

---
+++ main.py
@@ -0,0 +1,39 @@
+def sumar(a,b):
+    return a + b
+
+def restar(a, b):
+    return a - b
+
+
+def multiplicar(a, b):
+    return a * b
+
+def calcular(entrada):
+    #Borramos espacios
+    entrada = entrada.replace(" ","")
+    
+    if entrada.lower("c"):
+        return "Operación borrada"
+    
+    for operador in ["+","-","*"]:
+        if operador in entrada:
+            #Separamos las partes del string
+            partes = entrada.split(operador) 
+            if len(partes) == 2:
+                try:
+                    a = float(partes[0])
+                    b = float(partes[1])    
+                except ValueError:
+                    return "Error: Entrada inválida."
+                #Suma
+                if operador == '+':
+                    return sumar(a, b)
+                elif operador == '-':
+                    return restar(a, b)
+                elif operador == '*':
+                    return multiplicar(a, b)
+                
+while True:
+    entrada = input("Ingrese una operación (o 'c' para borrar): ")
+    resultado = calcular(entrada)
+    print(f"Resultado: {resultado}")
